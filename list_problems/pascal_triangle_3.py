7f0e377e331fe1ad5cf305ce337c74ad7daf5f3e
Mahir Patel <mahirpatel3107@gmail.com>
2025-11-05 11:33:12 -0600

    Solution for the problem number 3 of list

---
+++ list_problems/pascal_triangle_3.py
@@ -0,0 +1,26 @@
+from typing import List
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        pascal_list = [[1],[1,1]]
+        counter = 1
+        
+        if numRows == 1:
+            return [pascal_list[0]]
+            
+        for i in range(numRows - 2):
+            p_list = []
+            for j in range(len(pascal_list[counter])-1):
+                if j == 0:
+                   p_list.append(1)
+                p_list.append(pascal_list[counter][j] + pascal_list[counter][j+1])
+            p_list.append(1)
+            pascal_list.append(p_list)
+            counter +=1
+        return pascal_list
+    
+def main():
+    sol = Solution()
+    print(sol.generate(5))
+
+if __name__ == '__main__':
+    main()
