63f9fa6dafd79de72a0c414ec97073f6aa8407fe
Mahir Patel <mahirpatel3107@gmail.com>
2026-05-12 14:14:07 -0400

    Solve: 414, Third Maximum Number - Easy

---
+++ list_problems/third_maximum_num_414.py
@@ -0,0 +1,11 @@
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        pass
+            
+
+def main():
+    sol = Solution()
+    sol.thirdMax([3,2,1])
+
+if __name__ == '__main__':
+    main()
